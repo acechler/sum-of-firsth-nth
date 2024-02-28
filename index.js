31bdbc77607ea431a3f9a4cd553a1b52b00dd105
Aaron Echler <44124054+acechler@users.noreply.github.com>
2024-02-28 17:56:55 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/555eded1ad94b00403000071/train/javascript
+/**
+ *
+ * S[n] = sum of the first n terms
+ *    a = first term
+ *    d = the difference between each term
+ *    n = number of terms
+ * 
+ */
+
+
+function SeriesSum(n)
+{
+  // Happy Coding ^_^
+}
