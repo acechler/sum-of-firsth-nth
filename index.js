037f53590da28a4ba5ac676201544c997cdeb71b
Aaron Echler <44124054+acechler@users.noreply.github.com>
2024-02-28 18:02:00 -0500

    Add formula to repository

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // https://www.codewars.com/kata/555eded1ad94b00403000071/train/javascript
 /**
- *
+ * - Variables
  * S[n] = sum of the first n terms
  *    a = first term
  *    d = the difference between each term
  *    n = number of terms
+ * 
+ * - EQUATION
+ * S[n] = n/2(2a + (n-1)d)
  * 
  */
 
