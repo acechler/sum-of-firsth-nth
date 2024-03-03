4a5397333806816141efd451436cb5fff33abdc0
Aaron Echler <44124054+acechler@users.noreply.github.com>
2024-03-03 09:09:55 -0500

    Complete Challenge

--- index.js
+++ index.js
@@ -17,52 +17,25 @@
 
 function SeriesSum(n)
 {
-  if(n === 0){
-    return "0.00";
-  }
-  let total = Math.abs(1/(1+3*(1-1)));
   let container = "";
-  for(let i = 2; i <= n+1; i++){
+  if(n === 0){
+    container = "0.00";
+    return container;
+  }
+  if(n === 1){
+    container = "1.00";
+    return container;
+  }
+  let total = 1;
+  for(let i = 2; i <= n; i++){
     total+=Math.abs(1/(1+3*(i-1)))
-    let rounded = total.toFixed(2);
-    console.log(rounded.toString());
-    container = rounded.toString();
+    container = total.toFixed(2);;
   }
   return container;
 
 }
 
-//SeriesSum(1);
-SeriesSum(2);
-SeriesSum(0);
 
-//Math.abs(1/(1+3*(n-1)))
-
-/* // Initialize
-let total = Math.abs(1/(1+3*(1-1)));
-let i = 2;
-console.log(total);
-
-// First Iteration
-total += Math.abs(1/(1+3*(i-1)))
-i+=1;
-console.log(total);
-
-
-// Second Iteration
-total += Math.abs(1/(1+3*(i-1)))
-i+=1;
-console.log(total);
-
-
-// Third Iteration
-total += Math.abs(1/(1+3*(i-1)))
-i+=1;
-console.log(total);
-console.log('\n');
-
-let total2 = Math.abs(1/(1+3*(1-1)));
-for(let j = 2; j <= 3; j++){
-  total2+=Math.abs(1/(1+3*(j-1)))
-  console.log(total2);
-} */
+for(let i = 0; i < 15; i++){
+  console.log(SeriesSum(i));
+}
